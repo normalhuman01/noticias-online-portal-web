05453f1ac6079bbce326b58f1b7c49017f929991
Composta Puerta <adkadkwkq12@gmail.com>
2024-01-03 21:29:18 -0500

    upload
    commit push
    
    Co-Authored-By: Jesus Jair Alarcon Arca <36089570+jesus0508@users.noreply.github.com>
    Co-Authored-By: José Manuel Casani Guerra <21302114+mrjmpl3@users.noreply.github.com>

---
+++ client/astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), sitemap()],
+  redirects: {
+    "/": "/articles/1",
+    "/articles": "/articles/1",
+  },
+});
